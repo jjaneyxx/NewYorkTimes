375caec82b934861dab6862b3662f97459d73edf
jjaneyxx <hmyeonge@gmail.com>
2024-10-02 17:32:49 +0900

    feat: render() 함수로 뉴스 전체를 화면에 렌더링

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const API_KEY = `d0ecea3618044871bcbb7c6832d38e2b`;
-let news = []; // 자주 사용하므로 전역변수로 둠
+let newsList = []; // 자주 사용하므로 전역변수로 둠
 const getLatestNews = async () => {
   const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
   console.log("url:", url);
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("news", news);
+  newsList = data.articles;
+  render(); // newsList 가 확정된 이후 호출
+  console.log("newsList", newsList);
 };
 
 getLatestNews();
@@ -36,4 +37,22 @@ const openNav = () => {
 
 const closeNav = () => {
   document.getElementById("sideNav").style.width = "0";
+};
+
+// 뉴스 그리기
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row one-news">
+          <figure class="col-lg-4"><img class="article-img" src=${news.urlToImage} /></figure>
+          <article class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <p>${news.source.name} * ${news.publishedAt}</p>
+          </article>
+        </div>`
+    )
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
